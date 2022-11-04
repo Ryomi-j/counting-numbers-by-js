cab59e044d5d1b94abfb701b9423fb1c07bbac8c
Eunjeong Kim <ryomi.j78@gmail.com>
2022-11-04 20:07:10 +0900

    added a function showing numbers how many letters  i can write in a input element

--- demo.js
+++ demo.js
@@ -1,3 +1,13 @@
-let count = document.getElementById('remaining-chars');
-let input = document.getElementById('product-name');
+let remaining = document.getElementById('remaining-chars');
+let txtBox = document.getElementById('product-name');
+let possibleLength = txtBox.maxLength
 
+function updateRemainingChars(event) {
+    let enteredTxt = event.target.value;
+    let enteredTxtLength = enteredTxt.length;
+    
+    let remainingCharsCharacters = possibleLength - enteredTxtLength;
+    return remaining.textContent = remainingCharsCharacters;
+}
+
+txtBox.addEventListener('input', updateRemainingChars);
