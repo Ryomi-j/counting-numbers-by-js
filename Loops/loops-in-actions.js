97a878ca2daf2efef70a9501af94771cef4c6408
Eunjeong Kim <ryomi.j78@gmail.com>
2022-11-05 17:17:07 +0900

    added a statistics function

--- Loops/loops-in-actions.js
+++ Loops/loops-in-actions.js
@@ -41,7 +41,7 @@ const userDataBtn = document.querySelector("#user-data button");
 
 function addUserData() {
   const ulElement = document.getElementById("output-user-data");
-  ulElement.innerHTML = ""; // 기존 항목 제거
+  ulElement.innerHTML = "";
 
   for (const key in userData) {
     const listItem = document.createElement("li");
@@ -52,3 +52,44 @@ function addUserData() {
 }
 
 userDataBtn.addEventListener("click", addUserData);
+
+
+// 주사위로 특정 숫자를 얻을때 까지의 결과 반환
+const diceBtn = document.querySelector("#statistics button");
+
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function derivedNumberOfDiceRoll() {
+  const targetNumber = document.getElementById("user-target-number");
+  const ulElement = document.getElementById("dice-rolls");
+
+  const enteredNumber = targetNumber.value; //string
+  ulElement.innerHTML = "";
+
+  let hasRolledTargetNumber = false;
+  let numberOfRolls = 0;
+
+  while (!hasRolledTargetNumber) {
+    const rolledNumber = rollDice();
+    // if(rolledNumber == enteredNumber){
+    //     hasRolledTargetNumber = true;
+    // }
+    numberOfRolls++;
+    const newListItem = document.createElement("li");
+    const outputTxt = "Roll " + numberOfRolls + ": " + rolledNumber;
+    newListItem.textContent = outputTxt;
+    ulElement.append(newListItem);
+
+    hasRolledTargetNumber = rolledNumber == enteredNumber; //재할당
+  }
+
+  const totalRolls = document.getElementById("output-total-rolls");
+  const outputTargetNumber = document.getElementById("output-target-number");
+
+  totalRolls.textContent = numberOfRolls;
+  outputTargetNumber.textContent = enteredNumber;
+}
+
+diceBtn.addEventListener("click", derivedNumberOfDiceRoll);
