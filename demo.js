38331a058e3862954b202b5881073b8a74bd74fc
Eunjeong Kim <ryomi.j78@gmail.com>
2022-11-05 10:05:18 +0900

    added error and warning effects

--- demo.js
+++ demo.js
@@ -1,13 +1,25 @@
-let remaining = document.getElementById('remaining-chars');
-let txtBox = document.getElementById('product-name');
-let possibleLength = txtBox.maxLength
+let remaining = document.getElementById("remaining-chars");
+let txtBox = document.getElementById("product-name");
+let possibleLength = txtBox.maxLength;
 
 function updateRemainingChars(event) {
-    let enteredTxt = event.target.value;
-    let enteredTxtLength = enteredTxt.length;
-    
-    let remainingCharsCharacters = possibleLength - enteredTxtLength;
-    return remaining.textContent = remainingCharsCharacters;
+  let enteredTxt = event.target.value;
+  let enteredTxtLength = enteredTxt.length;
+  let remainingCharsCharacters = possibleLength - enteredTxtLength;
+  remaining.textContent = remainingCharsCharacters;
+
+  if (remainingCharsCharacters === 0) {
+    txtBox.classList.add("error");
+    remaining.classList.add("error");
+  } else if (remainingCharsCharacters < 10) {
+    txtBox.classList.remove("error");
+    remaining.classList.remove("error");
+    txtBox.classList.add("warning");
+    remaining.classList.add("warning");
+  } else {
+    txtBox.classList.remove("warning");
+    remaining.classList.remove("warning");
+  }
 }
 
-txtBox.addEventListener('input', updateRemainingChars);
+txtBox.addEventListener("input", updateRemainingChars);
